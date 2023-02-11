a170b1005c70879d31467acd917588c126d1f13e
Luke Smith <luke@lukesmith.xyz>
2023-02-11 11:40:38 -0500

    remove old junk

--- mailrm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Purge all configs from a previous attempt at a mail server.
-# Doesn't delete mail or anything like that.
-apt purge dovecot-core spamassassin postfix spamc opendkim
-
-# Some stragglers that often stay undeleted.
-rm -rf /etc/dovecot /var/lib/dovecot
